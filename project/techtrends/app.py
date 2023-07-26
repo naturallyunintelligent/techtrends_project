fb49e83c59c3284fe9477262f21c1585a4ac9213
Darren Steele <darren.steele@five.ai>
2023-07-26 13:06:10 +0100

    log at debug level, stderr & stdout, and fix post count to show total posts rather than posts added

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 import logging
+import sys
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -13,7 +14,7 @@ def get_db_connection():
     global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
-    app.logger.info('Connected to database.db')
+    app.logger.debug('Connected to database.db')
     db_connection_count += 1
     return connection
 
@@ -23,7 +24,7 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
-    app.logger.info(f'get_post:{post_id}')
+    app.logger.debug(f'get_post:{post_id}')
     return post
 
 # Define the Flask application
@@ -36,7 +37,8 @@ def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
-    app.logger.info('Main index.html loaded')
+    app.logger.debug('Main index.html loaded')
+    app.logger.debug(f'len(posts)={len(posts)}')
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered
@@ -45,16 +47,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-        app.logger.info('Returned 404 - post id not found')
+        app.logger.debug('Returned 404 - post id not found')
         return render_template('404.html'), 404
     else:
-        app.logger.info(f'`Retrieved post:{post_id}')
+        app.logger.debug(f'`Retrieved post:{post_id}')
         return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
-    app.logger.info('Retrieved /about page')
+    app.logger.debug('Retrieved /about page')
     return render_template('about.html')
 
 @app.route('/healthz/')
@@ -67,28 +69,29 @@ def status():
             status=500,
             mimetype='application/json'
         )
-        app.logger.info('db did not connect')
+        app.logger.debug('db did not connect')
         app.logger.debug('Can return some useful debug info here')
     response = app.response_class(
               response=json.dumps({"result":"OK - healthy"}),
               status=200,
               mimetype='application/json'
     )
-    app.logger.info('Successfully returned status')
+    app.logger.debug('Successfully returned status')
     app.logger.debug('Can return some useful debug info here')
     return response
 
 @app.route('/teapot/')
 def teapot():
-    app.logger.info('Successful teapot endpoint - for learning about endpoints')
+    app.logger.debug('Successful teapot endpoint - for learning about endpoints')
     return "Would you like some tea?", 418
 
 @app.route('/metrics/')
 def metrics():
-    global post_count
+    #global post_count
     global db_connection_count
     connection = get_db_connection()
     connection.close()
+    post_count = len(connection.execute('SELECT * FROM posts').fetchall())
     response = app.response_class(
           response=json.dumps({
                                "db_connection_count": db_connection_count,
@@ -96,14 +99,14 @@ def metrics():
           status=200,
           mimetype='application/json'
     )
-    app.logger.info('Successfully returned metrics response')
+    app.logger.debug('Successfully returned metrics response')
     return response
 
 
 # Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
-    global post_count
+    #global post_count
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
@@ -116,8 +119,8 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-            post_count += 1
-            app.logger.info(f'Successfully created post: {title}')
+            #post_count += 1
+            app.logger.debug(f'Successfully created post: {title}')
             return redirect(url_for('index'))
 
     return render_template('create.html')
@@ -125,5 +128,17 @@ def create():
 # start the application on port 3111
 if __name__ == "__main__":
     # Stream logs to a file, and set the default log level to DEBUG
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+    log_file_handler = logging.FileHandler(filename='app.log')
+    log_stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    log_stderr_handler = logging.StreamHandler(stream=sys.stderr)
+    log_handlers = [log_file_handler, log_stdout_handler, log_stderr_handler]
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=log_handlers
+    )
+
     app.run(host='0.0.0.0', port='3111')
